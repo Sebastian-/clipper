357339d85c3b37e231ef41462750c10cd9c1dffe
Sebastian- <hmurgu@hotmail.com>
2020-01-03 13:17:37 -0800

    Created and styled stub title search component

--- src/components/TitleSearch.js
+++ src/components/TitleSearch.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function TitleSearch(props) {
   const [query, setQuery] = useState("");
@@ -11,6 +13,7 @@ function TitleSearch(props) {
     <div className="title-search-container">
       <form>
         <label className="sr-only">Search</label>
+        <FontAwesomeIcon icon={faSearch} />
         <input
           value={query}
           onChange={handleInput}
