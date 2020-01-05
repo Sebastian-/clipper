ae879e5930fbbac24a63abf5c440cd8f1f0bc2a9
Sebastian- <hmurgu@hotmail.com>
2020-01-04 18:06:12 -0800

    Implement about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
 import React from "react";
+import tmdbLogo from "../img/tmdb.png";
 
 function About(props) {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="page-container">
+      <h2>About</h2>
+      <p>
+        This is a React-based application used to browse data provided by The
+        Movie Database (TMDb).
+      </p>
+      <p>
+        This product uses the TMDb API but is not endorsed or certified by TMDb.
+      </p>
+      <img src={tmdbLogo} alt="Powered by TMDb" />
     </div>
   );
 }
