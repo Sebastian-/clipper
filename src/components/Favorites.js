57d6e801a9ee2456f3aa031ed55db2205e600bc7
Sebastian- <hmurgu@hotmail.com>
2020-01-06 21:05:07 -0800

    Add handling for case when there are no favorited movies

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -3,10 +3,19 @@ import MovieList from "./MovieList";
 import { getFavorites } from "../utils/localStorageAPI";
 
 function Favorites(props) {
+  const movies = getFavorites();
+
   return (
     <div className="page-container">
-      <h2>Favorites</h2>
-      <MovieList movies={getFavorites()} />
+      <h2>Favorited Movies</h2>
+      {movies.length ? (
+        <MovieList movies={movies} />
+      ) : (
+        <p>
+          You haven't favorited any movies! Favorite a movie by visiting it's
+          'More Info' page.
+        </p>
+      )}
     </div>
   );
 }
